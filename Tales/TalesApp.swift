97097ea59142daf022c2496b3bc20e43cd083a9a
Antonio Carella <acarella@egencia.com>
2020-06-26 17:10:58 -0500

    Initial Commit

---
+++ Tales/TalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalesApp.swift
+//  Tales
+//
+//  Created by Antonio Carella on 6/26/20.
+//
+
+import SwiftUI
+
+@main
+struct TalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
